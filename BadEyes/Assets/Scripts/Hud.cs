8b916d4776086d1fa26640160ba5f6c7877e0792
HolyDeath <julien.vannier66@gmail.com>
2020-02-12 18:47:38 -0500

    Ajout myopie

---
+++ BadEyes/Assets/Scripts/Hud.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hud : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public void GenerateCube()
+    {
+        Instantiate(cubePrefab);
+    }
+}
